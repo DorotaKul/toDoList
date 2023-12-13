994a8a15bb70440ce069d436856816fd90118172
Dorota Kul <kuldorota1@gmail.com>
2023-12-13 13:23:26 +0100

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 {
-    
+
     const tasks = [
         {
             content: "nagrać lekcję",
-            done:false,
+            done: false,
 
         },
         {
@@ -11,24 +11,52 @@
             done: true,
         },
     ];
-    
-    const render = () => {
-    let htmlString = "";
 
-    for(const task of tasks) {
-        htmlString += `
-        <li>
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+        <li
+        ${task.done ? "style=\"text-decoration:line-through\"" : ""}
+        >
         ${task.content}
         </li>
         `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content:newTaskContent, 
+        });
+
+        render();
+
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    
+    if (newTaskContent === "") {
+        return;
     }
 
-    document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-    
-    const init = () =>{
-        render ();
+    addNewTask(newTaskContent);
 
+    
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form")
+
+        form.addEventListener("submit", onFormSubmit );
     };
 
     init();
