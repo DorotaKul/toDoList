fef9d6ce19f2106ae9108083b1c60e5131b33c05
Dorota Kul <kuldorota1@gmail.com>
2023-12-13 13:39:42 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -20,12 +20,23 @@
         <li
         ${task.done ? "style=\"text-decoration:line-through\"" : ""}
         >
+        <button class="js-remove">usuń</button>
         ${task.content}
         </li>
         `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+      
+        removeButtons.forEach((removeButton, index)=> {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+
+            })
+
+        });
     };
 
     const addNewTask = (newTaskContent) => {
@@ -36,6 +47,13 @@
         render();
 
     };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+                render();
+    };
+
+    
 
     const onFormSubmit = (event) => {
         event.preventDefault();
